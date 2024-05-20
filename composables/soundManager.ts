5fbe2f0a3849a3fc07dafa8744ada8fd887a8ce5
Sam McCarthy <samrmccarthy6@gmail.com>
2024-05-20 21:51:48 +1000

    Removed unused sounds/music

--- composables/soundManager.ts
+++ composables/soundManager.ts
@@ -45,17 +45,7 @@ export const useSoundManager = () => {
     soundManager.value.loadSound('boom', '/sounds/boom.mp3', 0.3, 1);
     soundManager.value.loadSound('click', '/sounds/click.wav', 0.8);
     soundManager.value.loadSound('switch', '/sounds/switch.wav', 0.1);
-    soundManager.value.loadSound('crickets', '/sounds/crickets.wav');
-    soundManager.value.loadSound('gameOver', '/sounds/game-over.wav');
-    soundManager.value.loadSound('item', '/sounds/item.wav');
-    soundManager.value.loadSound('levelUp', '/sounds/level-up.wav');
-    soundManager.value.loadSound('menu', '/sounds/menu.wav');
     soundManager.value.loadSound('metalDong', '/sounds/metal-dong.mp3', 0.2, 4);
-    soundManager.value.loadSound('newLife', '/sounds/new-life.mp3');
-    soundManager.value.loadSound('plop', '/sounds/plop.wav');
-    soundManager.value.loadSound('pop', '/sounds/pop.wav', 0.6);
-    soundManager.value.loadSound('win', '/sounds/win.wav');
-    soundManager.value.loadSound('won', '/sounds/won.wav');
     soundManager.value.loadSound('tileSwap', '/sounds/tile-swap.flac', 0.08);
     soundManager.value.loadSound('swoosh', '/sounds/swoosh.flac', 0.06);
     soundManager.value.loadSound('whoosh', '/sounds/whoosh.wav', 0.05);
